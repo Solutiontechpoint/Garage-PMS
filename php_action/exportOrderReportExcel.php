794d4e182465fa5a7394686c5de743a7042f1d7c
Thanu Ganesh <thanunr5@gmail.com>
2025-06-25 23:46:32 +0530

    Fix report issue

--- php_action/exportOrderReportExcel.php
+++ php_action/exportOrderReportExcel.php
@@ -6,17 +6,13 @@ if (isset($_GET['startDate']) && isset($_GET['endDate'])) {
     $endDate = $_GET['endDate'];
 
     header("Content-Type: application/vnd.ms-excel");
-    header("Content-Disposition: attachment; filename=Order_Report_{$startDate}_to_{$endDate}.xls");
+    header("Content-Disposition: attachment; filename=Combined_Report_{$startDate}_to_{$endDate}.xls");
 
-    $sql = "SELECT * FROM orders WHERE order_date >= '$startDate' AND order_date <= '$endDate' AND order_status = 1";
-    $query = $connect->query($sql);
-
-    echo '
-    <table border="1">
+    echo '<table border="1">
         <tr>
             <th>Bill No</th>
-            <th>Bill Date</th>
-            <th>Customer Party Name</th>
+            <th>Date</th>
+            <th>Customer Name</th>
             <th>Contact Number</th>
             <th>GST No</th>
             <th>HSN</th>
@@ -30,29 +26,64 @@ if (isset($_GET['startDate']) && isset($_GET['endDate'])) {
     $totalTaxable = 0;
     $totalTax = 0;
 
-    while ($result = $query->fetch_assoc()) {
-        $sql1 = "SELECT * FROM tbl_client WHERE id = '".$result['client_name']."'";
-        $result1 = $connect->query($sql1);
-        $data1 = $result1->fetch_assoc();
+    // ====== ORDERS ======
+    $orderQuery = $connect->query("SELECT * FROM orders WHERE order_date >= '$startDate' AND order_date <= '$endDate' AND order_status = 1");
+
+    while ($result = $orderQuery->fetch_assoc()) {
+        $clientQuery = $connect->query("SELECT * FROM tbl_client WHERE id = '".$result['client_name']."'");
+        $clientData = $clientQuery->fetch_assoc();
+
+        $vat = floatval($result['vat']);
+        $total_amount = floatval($result['total_amount']);
 
         echo '<tr>
             <td>'.$result['order_id'].'</td>
             <td>'.$result['order_date'].'</td>
-            <td>'.$data1['name'].'</td>
-            <td>'.$data1['mob_no'].'</td>
+            <td>'.$clientData['name'].'</td>
+            <td>'.$clientData['mob_no'].'</td>
             <td>'.$result['gst_no'].'</td>
             <td>'.$result['hsn'].'</td>
-            <td>'.$result['total_amount'].'</td>
+            <td>'.$total_amount.'</td>
             <td>18%</td>
-            <td>'.($result['vat']/2).'</td>
-            <td>'.($result['vat']/2).'</td>
+            <td>'.($vat / 2).'</td>
+            <td>'.($vat / 2).'</td>
             <td>0</td>
         </tr>';
 
-        $totalTaxable += $result['total_amount'];
-        $totalTax += $result['vat'];
+        $totalTaxable += $total_amount;
+        $totalTax += $vat;
     }
 
+    // ====== JOB CARDS ======
+    $jobCardQuery = $connect->query("SELECT * FROM job_card WHERE DATE(datetime_in) >= '$startDate' AND DATE(datetime_in) <= '$endDate' AND job_status = 1");
+
+    while ($row = $jobCardQuery->fetch_assoc()) {
+      
+        $clientQuery = $connect->query("SELECT * FROM tbl_client WHERE id = '".$row['customer_name']."'");
+        $clientData = $clientQuery->fetch_assoc();
+
+        $vat = floatval($row['vat']);
+        $total_amount = floatval($row['total_amount']);
+
+        echo '<tr>
+            <td>'.$row['job_card_id'].'</td>
+            <td>'.substr($row['datetime_in'], 0, 10).'</td>
+            <td>'.$clientData['name'].'</td>
+            <td>'.$clientData['mob_no'].'</td>
+            <td></td>
+            <td>'.$row['service_type'].'</td>
+            <td>'.$total_amount.'</td>
+            <td>18%</td>
+            <td>'.($vat / 2).'</td>
+            <td>'.($vat / 2).'</td>
+            <td>0</td>
+        </tr>';
+
+        $totalTaxable += $total_amount;
+        $totalTax += $vat;
+    }
+
+    // ====== TOTAL ROW ======
     echo '<tr>
         <td colspan="6"><strong>Total</strong></td>
         <td><strong>'.$totalTaxable.'</strong></td>
--- php_action/getOrderReport.php
+++ php_action/getOrderReport.php
@@ -6,10 +6,6 @@ if($_POST) {
 	$startDate = $_POST['startDate'];
 	$endDate = $_POST['endDate'];
 
-	$sql = "SELECT * FROM orders WHERE order_date >= '$startDate' AND order_date <= '$endDate' and order_status = 1";
-
-	$query = $connect->query($sql);
-
 	$table = '
 	<table border="1" cellspacing="0" cellpadding="5" style="width:100%; border-collapse: collapse;">
 		<tr>
@@ -27,34 +23,74 @@ if($_POST) {
 		</tr>';
 
 	$totalTaxable = 0;
-	while ($result = $query->fetch_assoc()) {
-		$sql1 = "SELECT * FROM tbl_client  
-		WHERE id = '".$result['client_name']."'";
+	$totalTax = 0;
 
-	  $result1 = $connect->query($sql1);
-	  $data1 = $result1->fetch_assoc();
+	// ===== ORDERS =====
+	$sql = "SELECT * FROM orders WHERE order_date >= '$startDate' AND order_date <= '$endDate' and order_status = 1";
+	$query = $connect->query($sql);
+
+	while ($result = $query->fetch_assoc()) {
+		$clientSql = "SELECT * FROM tbl_client WHERE id = '".$result['client_name']."'";
+		$clientRes = $connect->query($clientSql);
+		$client = $clientRes->fetch_assoc();
+
+		$vat = floatval($result['vat']);
+		$total = floatval($result['total_amount']);
 
 		$table .= '<tr>
 			<td><center>'.$result['order_id'].'</center></td>
 			<td><center>'.$result['order_date'].'</center></td>
-			<td><center>'.$data1['name'].'</center></td>
-		    <td><center>'.$data1['mob_no'].'</center></td>
+			<td><center>'.$client['name'].'</center></td>
+			<td><center>'.$client['mob_no'].'</center></td>
 			<td><center>'.$result['gst_no'].'</center></td>
 			<td><center>'.$result['hsn'].'</center></td>
-			<td><center>'.$result['total_amount'].'</center></td>
+			<td><center>'.$total.'</center></td>
 			<td><center>18%</center></td>
-			<td><center>'.($result['vat']/2).'</center></td>
-			<td><center>'.($result['vat']/2).'</center></td>
+			<td><center>'.($vat/2).'</center></td>
+			<td><center>'.($vat/2).'</center></td>
 			<td><center>0</center></td>
 		</tr>';
-		$totalTaxable += $result['total_amount'];
-		$totalTax += $result['vat'];
+
+		$totalTaxable += $total;
+		$totalTax += $vat;
 	}
 
+	// ===== JOB CARDS =====
+	$jobSql = "SELECT * FROM job_card WHERE DATE(datetime_in) >= '$startDate' AND DATE(datetime_in) <= '$endDate' AND job_status = 1";
+	$jobQuery = $connect->query($jobSql);
+
+	while ($row = $jobQuery->fetch_assoc()) {
+		$clientSql = "SELECT * FROM tbl_client WHERE id = '".$row['customer_name']."'";
+		$clientRes = $connect->query($clientSql);
+		$client = $clientRes->fetch_assoc();
+
+		$vat = floatval($row['vat']);
+		$total = floatval($row['total_amount']);
+
+		$table .= '<tr>
+			<td><center>'.$row['job_card_id'].'</center></td>
+			<td><center>'.substr($row['datetime_in'], 0, 10).'</center></td>
+			<td><center>'.$client['name'].'</center></td>
+			<td><center>'.$client['mob_no'].'</center></td>
+			<td><center></center></td>
+			<td><center>'.$row['service_type'].'</center></td>
+			<td><center>'.$total.'</center></td>
+			<td><center>18%</center></td>
+			<td><center>'.($vat/2).'</center></td>
+			<td><center>'.($vat/2).'</center></td>
+			<td><center>0</center></td>
+		</tr>';
+
+		$totalTaxable += $total;
+		$totalTax += $vat;
+	}
+
+	// ===== TOTAL ROW =====
 	$table .= '
 		<tr>
-			<td colspan="5"><center><strong>Total Value</strong></center></td>
-			<td colspan="3"><center><strong>'.$totalTaxable.'</strong></center></td>
+			<td colspan="6"><center><strong>Total Value</strong></center></td>
+			<td><center><strong>'.$totalTaxable.'</strong></center></td>
+			<td></td>
 			<td colspan="3"><center><strong>'.$totalTax.'</strong></center></td>
 		</tr>
 	</table>';
